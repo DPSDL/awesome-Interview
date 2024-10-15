df91f34bef67c744b09707dc968186efed1b5d73
dengpeng.dengpeng <dengpeng.dengpeng@alibaba-inc.com>
2024-10-15 11:34:32 +0800

    feat(sort): 添加快速排序算法实现
    - 引入 `math/rand` 包用于随机选择枢轴
    - 实现 `sortArray` 函数作为入口，返回排序后的数组
    - 实现 `quickSort` 函数进行递归排序
    - 实现 `pivotFunc` 函数选择并放置枢轴元素

--- leecode/sort/quickSort.go
+++ leecode/sort/quickSort.go
@@ -1,5 +1,41 @@
 package sort
 
-func quickSort(nums []int) {
+import "math/rand"
+
+func sortArray(nums []int) []int {
+	quickSort(nums, 0, len(nums)-1)
+
+	return nums
+}
+
+func quickSort(nums []int, left, right int) {
+	if left >= right {
+		return
+	}
+
+	pivot := pivotFunc(nums, left, right)
+
+	quickSort(nums, left, pivot-1)
+	quickSort(nums, pivot+1, right)
+
+	return
+}
+
+func pivotFunc(nums []int, left, right int) int {
+	pivotIndex := rand.Intn(right-left+1) + left
+
+	nums[pivotIndex], nums[right] = nums[right], nums[pivotIndex]
+	pivot := nums[right]
+	j := left
+
+	for i := left; i < right; i++ {
+		if nums[i] < pivot {
+			nums[j], nums[i] = nums[i], nums[j]
+			j++
+		}
+	}
+
+	nums[right], nums[j] = nums[j], nums[right]
+	return j
 
 }
